2d98863f2ce4f07b4d70a9130b545903c321fef6
tiernanraftery <G00418395@atu.ie>
2024-09-28 22:41:48 +0000

    2c arrow function takes 2 numbers and returns sum, print to console

---
+++ Lab1/2c.js
@@ -0,0 +1,8 @@
+
+//arrow function takes a 2 numbers and returns the sum
+let add = (num1, num2) => {
+    return num1 + num2;
+}
+
+//print out sum of numbers passed to arrow function
+console.log(add(10,20));
